47c6b6695cea4ffec7d1fc68dce55e7ac5242efe
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-18 00:49:42 +0200

    add success component

--- src/components/individual-account/Success.tsx
+++ src/components/individual-account/Success.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import Header from "../Header";
+import Tick from "./Tick";
 
 export default function Success() {
   return (
-    <div>Success</div>
-  )
+    <div className="grid place-items-center h-screen">
+      <div className="flex flex-col items-center gap-7 max-w-[25rem]">
+        <Tick />
+        <Header
+          textAlign="center"
+          title="Success"
+          description="You have received an email where you can read more about your account and setup your password."
+        />
+      </div>
+    </div>
+  );
 }
