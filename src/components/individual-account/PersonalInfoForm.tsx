2b8f916b1f7e7fbf23be5519271e6ea37a445eea
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-18 01:15:26 +0200

    validate personal-info form and update context with input data

--- src/components/individual-account/PersonalInfoForm.tsx
+++ src/components/individual-account/PersonalInfoForm.tsx
@@ -4,16 +4,60 @@ import TextInput from "../ui/TextInput";
 import PasswordInput from "../ui/PasswordInput";
 import CheckboxInput from "../ui/CheckboxInput";
 import FormSubmitButton from "../ui/FormSubmitButton";
-import { useForm } from "react-hook-form";
+import { FieldValues, useForm } from "react-hook-form";
+import { useAccountInfoContext } from "../../context/AccountInfoContext";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 type PersonalInfoFormProps = {
   setStep: React.Dispatch<React.SetStateAction<number>>;
 };
 
-export default function PersonalInfoForm({ setStep }: PersonalInfoFormProps) {
-  const { register } = useForm();
+const passwordValidation = new RegExp(
+  //one uppercase, one lowercase, one number, one special, at least 6 chars
+  /(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/
+);
 
-  function onSubmit() {
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Required" }),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(6, { message: "Must be at least 6 characters long" })
+    .regex(passwordValidation, {
+      message:
+        "Must include one uppercase letter, one lowercase letter and one special character.",
+    }),
+  terms: z.boolean().superRefine((terms, ctx) => {
+    if (!terms)
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Must agree to terms and conditions.",
+      });
+  }),
+});
+
+export default function PersonalInfoForm({ setStep }: PersonalInfoFormProps) {
+  const ctx = useAccountInfoContext();
+  if (!ctx) return;
+
+  const { name, email, password, terms } = ctx.info;
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name,
+      email,
+      password,
+      terms,
+    },
+  });
+
+  function onSubmit({ name, email, password, terms }: FieldValues) {
+    ctx?.setPersonalInfo({ name, email, password, terms });
     setStep((prev) => prev + 1);
   }
 
@@ -22,14 +66,14 @@ export default function PersonalInfoForm({ setStep }: PersonalInfoFormProps) {
       title="Register Individual Account!"
       description="For the purpose on industry regulation, your details are required."
     >
-      <form onSubmit={onSubmit} className="flex flex-col gap-10">
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-10">
         <TextInput
           name="name"
           placeholder="Enter full name"
           label="Your full name"
           required
           register={register}
-          error={""}
+          error={errors.name?.message}
         />
         <TextInput
           name="email"
@@ -37,7 +81,7 @@ export default function PersonalInfoForm({ setStep }: PersonalInfoFormProps) {
           label="Email address"
           required
           register={register}
-          error={""}
+          error={errors.email?.message}
         />
         <PasswordInput
           name="password"
@@ -45,11 +89,11 @@ export default function PersonalInfoForm({ setStep }: PersonalInfoFormProps) {
           label="Create password"
           required
           register={register}
-          error={""}
+          error={errors.password?.message}
         />
         <CheckboxInput
           name="terms"
-          error={""}
+          error={errors.terms?.message}
           label={<TermsLabel />}
           register={register}
         />
