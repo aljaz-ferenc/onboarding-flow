e58538ff0959f793ab8ac6a5a3cd4ae8dd0f1e0d
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-17 19:51:26 +0200

    add router with / and /individual routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import SignUpPage from "./pages/SignUpPage";
+import CreateIndividual from "./pages/CreateIndividual";
+
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <SignUpPage />,
+    },
+    {
+      path: "/individual",
+      element: <CreateIndividual />,
+    },
+  ]);
 
   return (
-    <>
-     App
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
---
+++ src/pages/CreateIndividual.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CreateIndividual() {
+  return (
+    <div>CreateIndividual</div>
+  )
+}
