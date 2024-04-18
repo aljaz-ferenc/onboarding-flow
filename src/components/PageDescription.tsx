90f8755145b4e0c785a67ce29caf73ece578b90a
Aljaž Ferenc <aljaz.ferenc@protonmail.com>
2024-04-18 03:25:01 +0200

    animate page description on text change

--- src/components/PageDescription.tsx
+++ src/components/PageDescription.tsx
@@ -1,15 +1,33 @@
+import { AnimatePresence, motion } from "framer-motion";
+
 type PageDescriptionProps = {
   text: string;
 };
 
 export default function PageDescription({ text }: PageDescriptionProps) {
   return (
-    <div className="w-[70%] ">
-      <img src='/public/quote.svg' className="block mb-5"/>
-      <p className=" mx-auto text-xl leading-10 text-white relative">{text}
-      <img src="/public/square.svg" className="absolute right-0 bottom-0 translate-y-[100%]"/>
-      <img src="/public/dots.svg" className="absolute top-0 right-0 translate-y-[-300%] translate-x-[-80%]"/>
-      </p>
-    </div>
+    <AnimatePresence mode="wait">
+      <motion.div
+        className="w-[70%] "
+        key={text}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ ease: "linear", duration: 0.2 }}
+      >
+        <img src="/public/quote.svg" className="block mb-5" />
+        <p className=" mx-auto text-xl leading-10 text-white relative">
+          {text}
+          <img
+            src="/public/square.svg"
+            className="absolute right-0 bottom-0 translate-y-[100%]"
+          />
+          <img
+            src="/public/dots.svg"
+            className="absolute top-0 right-0 translate-y-[-300%] translate-x-[-80%]"
+          />
+        </p>
+      </motion.div>
+    </AnimatePresence>
   );
 }
